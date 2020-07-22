ee5e03fbf4efc19ff2135f42e5348e877b40c10e
yuseok <znql16@gmail.com>
2020-07-22 10:39:50 +0900

    스토어의 mutations에서 범용적으로 사용하는 mutation으로 data 관리하기.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -30,12 +30,7 @@ export default {
         }
     },
     methods: {
-        async searchMovies () {
-            this.loading = true
-            const res = await axios.get(`http://www.omdbapi.com/?apikey=39ea34de&s=${this.title}`) // 서버에 요청을 보내면 Promise 객체가 반환 된다.
-            console.log(res.data)
-            this.loading = false
-        }
+
     }
 }
 </script>
--- src/store/movie.js
+++ src/store/movie.js
@@ -7,13 +7,26 @@ export default {
     }),
     getters: {},
     // 비동기처리 가능하지 않다.
-    mutations: {},
+    mutations: {
+        // 범용적으로 사용하는 mutation
+        updateState (state, payload) {
+            // payload는 객체 데이터이고 객체 데이터의 key 값(property)을 문자로 만들 수 있는 구조로 만든다.
+            Object.keys(payload).forEach(key => {
+                state[key] = payload[key] // (좌)sate에 있는 loading = (우) payload에 있는 loading
+            })
+        }
+    },
     // 비동기처리 가능하다.
     actions: {
-        async searchMovies ({ state }) { // context에 있는 state를 바로 할당.
-            state.loading = true // actions에서 데이터를 바로 할당하면 안된다. (mutations의 도움을 받아야함.)
+        async searchMovies ({ state, commit }) { //commit을 사용하기 위해서 2번째 인수로 받는다.
+            // state.loading = true
+            commit('updateState', { //mutation을 이용해 state의 loading 부분에 true 값 할당시킨다. 
+                loading: true
+            })
             const res = await axios.get(`http://www.omdbapi.com/?apikey=39ea34de&s=${state.title}`)
-            state.loading = false
+            commit('updateState', { //mutation을 이용해 state의 loading 부분에 true 값 할당시킨다. 
+                loading: false
+            })
         }
     }
 }
