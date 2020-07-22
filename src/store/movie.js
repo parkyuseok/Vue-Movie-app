03cf30029e7042774ab727f7c3dfb5609d472405
yuseok <znql16@gmail.com>
2020-07-22 10:17:40 +0900

    movie.js data 이관

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,1 +1,11 @@
 // movie.js는 저장소의 일종의 모듈이다. 따라서 index.js에서 import해서 사용해야한다.
+export default {
+    // state 빼고 복수, state는 일종의 데이터이다. => arrow function 사용.
+    state: () => ({
+        title: '',
+        loading: false
+    }),
+    getters: {},
+    mutations: {},
+    actions: {}
+}
